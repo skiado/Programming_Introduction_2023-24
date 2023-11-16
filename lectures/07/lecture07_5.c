627cd0be97ab0715bef391cf4b577a183ee60119
skiado <70379186+skiado@users.noreply.github.com>
2023-11-16 16:43:43 +0200

    Update lecture07_5.c

--- lectures/07/lecture07_5.c
+++ lectures/07/lecture07_5.c
@@ -9,7 +9,7 @@ struct time {
 
 int main (void){
 	struct time timeUpdate (struct time now);
-	struct time testTimes[5] = { { 11, 59, 59 }, { 12, 0, 0 }, { 1, 29, 59 },	{ 23, 59, 59 }, { 19, 12, 270 }};
+	struct time testTimes[5] = { { 11, 59, 59 }, { 12, 0, 0 }, { 1, 29, 59 },{ 23, 59, 59 }, { 19, 12, 270 }};
 	
 	for (int i = 0; i < 5; i++ ) {
 		printf ("Time is %.2i:%.2i:%.2i", testTimes[i].hour,testTimes[i].minutes, testTimes[i].seconds);
